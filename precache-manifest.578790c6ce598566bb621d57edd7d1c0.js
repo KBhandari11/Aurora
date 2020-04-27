154e1998ef83835d9e99f9cbcc9398a14a495b01
Kushal Bhandari <kushal@Kushals-MacBook-Pro.local>
2020-04-26 21:39:05 -0400

    Updates

---
+++ precache-manifest.578790c6ce598566bb621d57edd7d1c0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca94338163612cdc95939acce154c5bc",
+    "url": "/Aurora/index.html"
+  },
+  {
+    "revision": "6db97ece9637657fb4c0",
+    "url": "/Aurora/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "aa5110cde250e8ba7a64",
+    "url": "/Aurora/static/js/2.e7c1c348.chunk.js"
+  },
+  {
+    "revision": "8bc88370bc2489d86f2f357f447ddb0b",
+    "url": "/Aurora/static/js/2.e7c1c348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6db97ece9637657fb4c0",
+    "url": "/Aurora/static/js/main.cc458fa3.chunk.js"
+  },
+  {
+    "revision": "7f56ebc8aac29147355d",
+    "url": "/Aurora/static/js/runtime-main.c29ad3a0.js"
+  },
+  {
+    "revision": "2e6c88ddc4f4b136e8a9619c2d70251b",
+    "url": "/Aurora/static/media/heatmap_1.2e6c88dd.svg"
+  },
+  {
+    "revision": "383c3e506d3b744219090309ffc37d4d",
+    "url": "/Aurora/static/media/heatmap_1.383c3e50.png"
+  },
+  {
+    "revision": "0806560d2d24ebc582ca7ca1fb49a5a8",
+    "url": "/Aurora/static/media/heatmap_2.0806560d.svg"
+  },
+  {
+    "revision": "58117b7aefb61d4d1ca2db8c3d147873",
+    "url": "/Aurora/static/media/heatmap_2.58117b7a.png"
+  },
+  {
+    "revision": "26d7c5f504b99245df73863ba398a30c",
+    "url": "/Aurora/static/media/heatmap_3.26d7c5f5.svg"
+  },
+  {
+    "revision": "31a44b27f1334cc996e34982a3444ca9",
+    "url": "/Aurora/static/media/heatmap_3.31a44b27.png"
+  },
+  {
+    "revision": "3c767dc55c11478c25e0033d13f5df75",
+    "url": "/Aurora/static/media/heatmap_4.3c767dc5.svg"
+  },
+  {
+    "revision": "f41d438cee23304805d086f1bf1eac1f",
+    "url": "/Aurora/static/media/heatmap_4.f41d438c.png"
+  },
+  {
+    "revision": "0de0109add4cd48b218ccc5ef0f5bc57",
+    "url": "/Aurora/static/media/heatmap_5.0de0109a.png"
+  },
+  {
+    "revision": "120abc5c087e8dd332a2e4d2f6d727a1",
+    "url": "/Aurora/static/media/heatmap_5.120abc5c.svg"
+  },
+  {
+    "revision": "93c51184c7e13647c1767cf0a1a2354a",
+    "url": "/Aurora/static/media/nav.93c51184.png"
+  }
+]);
